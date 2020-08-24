c8123e636fddded1fbc80a79d8ad7067d358a3c3
hrissolo <hrissolo@gmail.com>
2020-08-24 13:42:03 -0500

    missed a city

--- scripts/thailand/landmarks/LandmarkList.js
+++ scripts/thailand/landmarks/LandmarkList.js
@@ -5,16 +5,16 @@ export const landmarkList = () => {
     const element = document.querySelector(".thailandMain")
     const landmarks = useLandmarks()
 
-    let cityHTMLRep = "";
+    let landmarkHTMLRep = "";
 
     for (const landmark of landmarks){
-            cityHTMLRep += Landmark(landmark)
+            landmarkHTMLRep += Landmark(landmark)
     }
 
 element.innerHTML+= `
     <section class="landmarkList">
         <h2>Landmarks</h2>
-        ${cityHTMLRep}
+        ${landmarkHTMLRep}
     </section>
     `
 }
