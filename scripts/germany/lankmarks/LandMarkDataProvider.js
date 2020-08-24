517fb742a5a0d1821b99df5661e43c249072af49
Travis Pinson <travispinson@gmail.com>
2020-08-24 10:30:43 -0500

    filled in city data

---
+++ scripts/germany/lankmarks/LandMarkDataProvider.js
@@ -0,0 +1,18 @@
+const lankmarkArray = [
+    {
+        name: "Neuschwanstein Castle",
+        location: "Bavaria"
+    },
+    {
+        name: "Brandenburn Gate",
+        location: "Berlin"
+    },
+    {
+        name: "Reichstag Building",
+        location: "Berlin"
+    },
+    {
+        name: "Cologne Cathedral",
+        location: "Cologne"
+    }
+]
