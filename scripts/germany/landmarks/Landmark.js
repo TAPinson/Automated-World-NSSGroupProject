7da12ce71326645bd5577b80656b2a5c8f4c74f4
Travis Pinson <travispinson@gmail.com>
2020-08-24 13:43:39 -0500

    corrected a type

--- scripts/germany/landmarks/Landmark.js
+++ scripts/germany/landmarks/Landmark.js
@@ -1,6 +1,6 @@
 export const Landmark = (landmarkObj) => {
     return `
-    <section class="cityList-city">
+    <section class="landmarkList-landmark">
         <h3> ${landmarkObj.name} </h3>
         <p> Location: ${landmarkObj.location}</p>
     </section>
