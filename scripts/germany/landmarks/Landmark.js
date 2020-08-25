52600244abac83106083a512bc03f9bddf060b02
Travis Pinson <travispinson@gmail.com>
2020-08-25 11:22:08 -0500

    implemented search bar for germany

--- scripts/germany/landmarks/Landmark.js
+++ scripts/germany/landmarks/Landmark.js
@@ -1,6 +1,6 @@
 export const Landmark = (landmarkObj) => {
     return `
-    <section class="landmarkList-landmark">
+    <section class="landmarkList-landmark search">
         <h3> ${landmarkObj.name} </h3>
         <p> Location: ${landmarkObj.location}</p>
     </section>
--- scripts/germany/leaders/Leader.js
+++ scripts/germany/leaders/Leader.js
@@ -1,6 +1,6 @@
 export const Leader = (leaderObj) => {
     return `
-    <section class="leaderList-leader">
+    <section class="leaderList-leader search">
         <h3> ${leaderObj.name} </h3>
         <p> Origin: ${leaderObj.origin}</p>
     </section>
